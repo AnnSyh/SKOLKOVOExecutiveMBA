4ae6991dfc188700fff0d40a32aec2a29b4e226d
AnnSyh <ann.syharevskaya@gmail.com>
2026-02-05 20:42:29 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function ()
+{
+	const menuBtn = document.querySelector("button.humburger");
+	if (menuBtn)
+	{
+		menuBtn.addEventListener("click", () =>
+		{
+			const menu = document.querySelector(".mobile-menu");
+			if (menu)
+			{
+				menu.classList.toggle("hidden");
+			}
+		});
+	}
+});
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./src/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        '16': 'repeat(16, minmax(0, 1fr))', // Определяем 16 колонки
+      },
+      gridColumn: {
+        'span-16': 'span 16 / span 16', // Добавляем кастомный класс col-span-18
+        'span-14': 'span 14 / span 14', // Добавляем кастомный класс col-span-14
+        'span-13': 'span 13 / span 13', // Добавляем кастомный класс col-span-13
+        'span-7': 'span 7 / span 7', // Добавляем кастомный класс col-span-7
+        'span-6': 'span 6 / span 6', // Добавляем кастомный класс col-span-6
+        'span-5': 'span 5 / span 5', // Добавляем кастомный класс col-span-5
+        'span-4': 'span 4 / span 4', // Добавляем кастомный класс col-span-4
+        'span-3': 'span 3 / span 3', // Добавляем кастомный класс col-span-3
+      },
+      backdropBlur: {
+        'custom': '28.6px',// backdrop-blur-custom
+      },
+      fontFamily: {
+        sans: [ 'Raleway', 'sans-serif' ],
+      },
+      colors: {
+        primary: '#ffffff',
+        secondary: '#10182A',
+        tertiary: '#F7F7F7',
+        violet: '#480CA8',
+        bgviolet: '#8B79D9',
+        gray: {
+          DEFAULT: '#656462',// bg-gray
+          dark: '#3d3b3c',   // bg-gray-dark
+        },
+        rosy: {
+          DEFAULT: '#fffcfb',// bg-rosy
+          dark: '#fff5f7', // bg-rosy-dark
+        },
+      },
+      backgroundImage: {
+        'gradient-to-br': 'linear-gradient(to bottom right, var(--tw-gradient-stops))',
+      },
+
+    },
+  },
+  plugins: [],
+};
